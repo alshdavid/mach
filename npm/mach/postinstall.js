8aa406a64c28fe4097d34eddfe835cd0a6f36cb0
David Alsh <alshdavid@gmail.com>
2024-02-24 19:34:08 +1100

    pipeline

---
+++ npm/mach/postinstall.js
@@ -0,0 +1,7 @@
+const packageJson = require('./package.json')
+if (packageJson.version === '0.0.0' || process.env.MACH_SKIP_INSTALL === 'true') {
+  console.log('skip download')
+  process.exit(0) 
+}
+
+console.log('download')
