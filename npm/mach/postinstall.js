b34d9ba798952c80c95876267c1dee8f156c5a6f
David Alsh <alshdavid@gmail.com>
2024-02-24 19:42:59 +1100

    pipeline

--- npm/mach/postinstall.js
+++ npm/mach/postinstall.js
@@ -1,7 +1,9 @@
-const packageJson = require('./package.json')
-if (packageJson.version === '0.0.0' || process.env.MACH_SKIP_INSTALL === 'true') {
-  console.log('skip download')
-  process.exit(0) 
-}
+require('fs').writeFileSync('./foo', '#!/bin/bash\necho hello', 'utf8')
 
-console.log('download')
+// const packageJson = require('./package.json')
+// if (packageJson.version === '0.0.0' || process.env.MACH_SKIP_INSTALL === 'true') {
+//   console.log('skip download')
+//   process.exit(0) 
+// }
+
+// console.log('download')
