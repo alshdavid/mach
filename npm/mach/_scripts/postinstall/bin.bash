f2568a4a040a37a33e71c0204a2cdca073b4e97d
David Alsh <alshdavid@gmail.com>
2024-02-26 21:02:39 +1100

    added version logging

--- npm/mach/_scripts/postinstall/bin.bash
+++ npm/mach/_scripts/postinstall/bin.bash
@@ -1,8 +1,16 @@
 #!/bin/sh
+set -e
+
+TARGET_PATH=$(node -e "const { dirname } = require('node:path'); console.log(dirname(dirname(require.resolve('@alshdavid/mach'))))")
+BIN_PATH="$TARGET_PATH/mach/bin/mach"
 
 if [ "$MACH_BINARY_PATH" != "" ]; then
   $MACH_BINARY_PATH $@
+fi
+
+if [[ "$@" = *"--version"* ]]; then
+  node -e "console.log('NPM Package:', JSON.parse(require('node:fs').readFileSync(require('node:path').join('${TARGET_PATH}', 'package.json'))).version)"
+  echo "Mach Binary: $($BIN_PATH --version)"
 else
-  TARGET_PATH=$(node -e "const { dirname } = require('path'); console.log(dirname(dirname(require.resolve('@alshdavid/mach'))))")
-  $TARGET_PATH/mach/bin/mach $@
+  $BIN_PATH $@
 fi
