4d4bf3bcfc1e7de3760b78f0b124e08df78b36f8
David Alsh <alshdavid@gmail.com>
2024-03-03 21:06:15 +1100

    serve settings

--- mach/src/cmd/serve/config.rs
+++ mach/src/cmd/serve/config.rs
@@ -1,4 +1,29 @@
+use std::path::PathBuf;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-pub struct ServeCommand {}
+pub struct ServeCommand {
+  /// The folder to serve
+  pub target: PathBuf,
+
+  /// Host address
+  #[arg(long = "host", default_value = "127.0.0.1")]
+  pub host: String,
+
+  /// Port to serve on
+  #[arg(short = 'p', long = "port")]
+  pub port: usize,
+
+  /// Allow cors requests
+  #[arg(long = "cors")]
+  pub cors: bool,
+
+  /// SSL Cert
+  #[arg(short = 'S', long = "ssl-cert")]
+  pub ssl_cert: Option<String>,
+
+  /// SSL Key
+  #[arg(short = 's', long = "ssl-key")]
+  pub ssl_key: Option<String>,
+}
--- mach/src/cmd/serve/main.rs
+++ mach/src/cmd/serve/main.rs
@@ -2,11 +2,11 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 
 use bytes::Bytes;
-use http_body_util::{Full};
+use http_body_util::Full;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
-use hyper_util::rt::{TokioIo, TokioTimer};
+use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
 use super::ServeCommand;
@@ -15,35 +15,17 @@ async fn hello(_: Request<impl hyper::body::Body>) -> Result<Response<Full<Bytes
   Ok(Response::new(Full::new(Bytes::from("Hello World!"))))
 }
 
-async fn main_async(command: ServeCommand) {
-  // This address is localhost
+async fn main_async(_command: ServeCommand) {
   let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
-
-  // Bind to the port and listen for incoming TCP connections
   let listener = TcpListener::bind(addr).await.unwrap();
   println!("Listening on http://{}", addr);
+
   loop {
-      // When an incoming TCP connection is received grab a TCP stream for
-      // client<->server communication.
-      //
-      // Note, this is a .await point, this loop will loop forever but is not a busy loop. The
-      // .await point allows the Tokio runtime to pull the task off of the thread until the task
-      // has work to do. In this case, a connection arrives on the port we are listening on and
-      // the task is woken up, at which point the task is then put back on a thread, and is
-      // driven forward by the runtime, eventually yielding a TCP stream.
       let (tcp, _) = listener.accept().await.unwrap();
-      // Use an adapter to access something implementing `tokio::io` traits as if they implement
-      // `hyper::rt` IO traits.
       let io = TokioIo::new(tcp);
 
-      // Spin up a new task in Tokio so we can continue to listen for new TCP connection on the
-      // current task without waiting for the processing of the HTTP1 connection we just received
-      // to finish
       tokio::task::spawn(async move {
-          // Handle the connection from the client using HTTP1 and pass any
-          // HTTP requests received on that connection to the `hello` function
           if let Err(err) = http1::Builder::new()
-              .timer(TokioTimer)
               .serve_connection(io, service_fn(hello))
               .await
           {
