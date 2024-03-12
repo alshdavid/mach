d88d6e0c3b7c96bb9e71528e05187839f89d2299
David Alsh <alshdavid@gmail.com>
2024-03-12 19:39:37 +1100

    success

--- _old/src/deno_cli/ops/mod.rs
+++
@@ -1,5 +0,0 @@
-// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
-
-pub mod bench;
-pub mod jupyter;
-pub mod testing;
--- vendor/deno_embed/src/lib.rs
+++ vendor/deno_embed/src/lib.rs
@@ -1,5 +1,53 @@
 mod snapshots;
+mod deno_current_thread;
+mod deno_init_options;
+mod deno_init;
 
+// Reexport Deno's API
 pub mod deno_cli;
+pub use deno_ast;
+pub use deno_broadcast_channel;
+pub use deno_cache;
+pub use deno_canvas;
+pub use deno_console;
+pub use deno_core;
+pub use deno_cron;
+pub use deno_cache_dir;
+pub use deno_crypto;
+pub use deno_fetch;
+pub use deno_ffi;
+pub use deno_fs;
+pub use deno_http;
+pub use deno_io;
+pub use deno_kv;
+pub use deno_napi;
+pub use deno_net;
+pub use deno_node;
+pub use deno_runtime;
+pub use deno_tls;
+pub use deno_lockfile;
+pub use deno_terminal;
+pub use deno_url;
+pub use deno_task_shell;
+pub use deno_web;
+pub use deno_webgpu;
+pub use deno_emit;
+pub use deno_webidl;
+pub use deno_websocket;
+pub use deno_doc;
+pub use deno_webstorage;
+pub use deno_graph;
+pub use deno_semver;
+pub use deno_lint;
+pub use deno_npm;
+pub use deno_config;
+
+pub use deno_core::url::Url;
+pub use deno_core::ModuleCodeString;
+pub use deno_core::ModuleSpecifier;
+
+pub use self::deno_current_thread::*;
+pub use self::deno_init_options::*;
+pub use self::deno_init::*;
 
 pub const DENO_VERSION: &str = "1.42.1";
