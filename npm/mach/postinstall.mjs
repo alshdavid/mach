9771abc0ee9e10b5238c7ec593df040ff1f54b4a
David Alsh <alshdavid@gmail.com>
2024-04-02 23:08:16 +1100

    workflow

--- npm/mach/postinstall.mjs
+++ npm/mach/postinstall.mjs
@@ -6,6 +6,10 @@ import * as url from "node:url"
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 const require = createRequire(import.meta.url);
 
+if (JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')).version === "0.0.0-local") {
+  process.exit(0)
+}
+
 const OS = {
   'win32': 'windows',
   'darwin': 'macos',
@@ -28,7 +32,6 @@ if (ARCH && OS) {
   console.warn('Could not find Mach binary for your system. Please compile from source')
   console.warn('Override the built in binary by setting the $MACH_BIN_PATH_OVERRIDE environment variable')
 }
-
 
 const pwsh = `
 @echo off
