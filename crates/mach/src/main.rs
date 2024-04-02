f775d517275c0ff454445ad4210046a125c960fb
David Alsh <alshdavid@gmail.com>
2024-04-03 07:19:10 +1100

    workflow

--- crates/mach/src/main.rs
+++ crates/mach/src/main.rs
@@ -1,67 +1,63 @@
-fn main() {
-  println!("hello world");
+#![deny(unused_crate_dependencies)]
+
+mod cmd;
+mod kit;
+mod platform;
+
+use std::time::SystemTime;
+
+use clap::Parser;
+use clap::Subcommand;
+use cmd::build::BuildCommand;
+use cmd::dev::DevCommand;
+use cmd::version::VersionCommand;
+use cmd::watch::WatchCommand;
+
+/*
+  Main just acts as a router to run CLI commands
+*/
+
+#[derive(Parser, Debug)]
+struct Commands {
+  #[clap(subcommand)]
+  command: CommandType,
 }
 
-// #![deny(unused_crate_dependencies)]
+#[derive(Debug, Subcommand)]
+pub enum CommandType {
+  /// Build a project
+  Build(BuildCommand),
+  /// Start a web server and reload when changes are detected
+  Dev(DevCommand),
+  /// Build and rebuild when changes are detected
+  Watch(WatchCommand),
+  /// Print version information
+  Version(VersionCommand),
+}
 
-// mod cmd;
-// mod kit;
-// mod platform;
-
-// use std::time::SystemTime;
-
-// use clap::Parser;
-// use clap::Subcommand;
-// use cmd::build::BuildCommand;
-// use cmd::dev::DevCommand;
-// use cmd::version::VersionCommand;
-// use cmd::watch::WatchCommand;
-
-// /*
-//   Main just acts as a router to run CLI commands
-// */
-
-// #[derive(Parser, Debug)]
-// struct Commands {
-//   #[clap(subcommand)]
-//   command: CommandType,
-// }
-
-// #[derive(Debug, Subcommand)]
-// pub enum CommandType {
-//   /// Build a project
-//   Build(BuildCommand),
-//   /// Start a web server and reload when changes are detected
-//   Dev(DevCommand),
-//   /// Build and rebuild when changes are detected
-//   Watch(WatchCommand),
-//   /// Print version information
-//   Version(VersionCommand),
-// }
-
-// fn main() {
-//   let start_time = SystemTime::now();
+fn main() {
+  let start_time = SystemTime::now();
   
-//   {
-//     let command = Commands::parse();
+  {
+    let command = Commands::parse();
 
-//     match command.command {
-//       CommandType::Build(command) => {
-//         if let Err(msg) = cmd::build::main(command) {
-//           println!("Build Error\n{}", msg);
-//         };
-//       }
-//       CommandType::Dev(command) => {
-//         cmd::dev::main(command);
-//       }
-//       CommandType::Watch(command) => {
-//         cmd::watch::main(command);
-//       }
-//       CommandType::Version(_) => {
-//         cmd::version::main();
-//       }
-//     }
-//   }
+    match command.command {
+      CommandType::Build(command) => {
+        if let Err(msg) = cmd::build::main(command) {
+          println!("Build Error\n{}", msg);
+        };
+      }
+      CommandType::Dev(command) => {
+        cmd::dev::main(command);
+      }
+      CommandType::Watch(command) => {
+        cmd::watch::main(command);
+      }
+      CommandType::Version(_) => {
+        cmd::version::main();
+      }
+    }
+  }
 
-//   println!("Total Time:    {:.3}s", start_time.elapsed().unwrap().as_nanos() as f64 / 1_000_000 as f64 / 1000 as f64);
-// }
+  println!("Total Time:    {:.3}s", start_time.elapsed().unwrap().as_nanos() as f64 / 1_000_000 as f64 / 1000 as f64);
+}
