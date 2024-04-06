cb42c168a33fb8bad2b0c14ac966ec848c77b9f8
David Alsh <alshdavid@gmail.com>
2024-04-06 17:30:32 +1100

    workflow

---
+++ .github/workflows/release/linux-amd64.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIR=$(dirname $(dirname $(dirname $SCRIPT_DIR)))
+
+NODE_VERSION=$(cat $ROOT_DIR/.nvmrc | sed -z '$ s/\n//')
+bash $ROOT_DIR/.github/workflows/platform/install-nodejs.bash $NODE_VERSION
+
