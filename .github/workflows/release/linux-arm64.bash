722f4e16bb96e47b175a5eb69ccd86f07861d820
David Alsh <alshdavid@gmail.com>
2024-04-06 17:46:33 +1100

    workflow

--- .github/workflows/release/linux-arm64.bash
+++ .github/workflows/release/linux-arm64.bash
@@ -4,7 +4,7 @@ set -ev
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIR=$(dirname $(dirname $(dirname $SCRIPT_DIR)))
 
-NODE_VERSION=$(cat $ROOT_DIR/.nvmrc | sed -z '$ s/\n//')
+NODE_VERSION=$(cat $ROOT_DIR/.nvmrc | sed -E '$ s/\n//g')
 bash $ROOT_DIR/.github/workflows/platform/install-nodejs.bash $NODE_VERSION
 node -v
 
