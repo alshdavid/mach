814faf3f081777ee92497047b88624ae8225ced9
David Alsh <alshdavid@gmail.com>
2024-04-06 17:33:42 +1100

    workflow

--- .github/workflows/release/macos-amd64.bash
+++ .github/workflows/release/macos-amd64.bash
@@ -1,3 +1,12 @@
 #!/bin/bash
+set -ev
 
-echo hi
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIR=$(dirname $(dirname $(dirname $SCRIPT_DIR)))
+
+NODE_VERSION=$(cat $ROOT_DIR/.nvmrc | sed -z '$ s/\n//')
+bash $ROOT_DIR/.github/workflows/platform/install-nodejs.bash $NODE_VERSION
+node -v
+
+bash $ROOT_DIR/.github/workflows/platform/install-rust.bash $NODE_VERSION
+cargo -v
