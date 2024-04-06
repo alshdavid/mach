a89f83cd429866436f25916305457755f2443c77
David Alsh <alshdavid@gmail.com>
2024-04-06 19:36:06 +1100

    workflow

--- .github/workflows/platform/unix/install-rust.bash
+++ .github/workflows/platform/unix/install-rust.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
+
 export RUSTUP_HOME=$HOME/.local/rust/rustup
 export CARGO_HOME=$HOME/.local/rust/cargo
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
+echo "RUSTUP_HOME=$HOME/.local/rust/rustup" >> $GITHUB_ENV
+echo "CARGO_HOME=$HOME/.local/rust/cargo" >> $GITHUB_ENV
 
 export PATH="$HOME/.local/rust/cargo/bin:$PATH"
 echo "${HOME}/.local/rust/cargo/bin" >> $GITHUB_PATH
