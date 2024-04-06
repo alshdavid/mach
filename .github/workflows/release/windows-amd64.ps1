cb6fa0b63b9b15cbb380e391db071eaf52b35e06
David Alsh <alshdavid@gmail.com>
2024-04-06 23:03:31 +1100

    workflow

--- .github/workflows/release/windows-amd64.ps1
+++ .github/workflows/release/windows-amd64.ps1
@@ -1,1 +1,22 @@
+$RootPath = @(Split-Path @(Split-Path @(Split-Path $pwd.Path)))
+$Job = "windows-amd64"
+
 & "$PSScriptRoot\..\platform\windows\setup.ps1"
+
+New-Item -ItemType "directory" -Force -Path "$RootPath\artifacts" | Out-Null
+
+npm install -g npm pnpm
+pnpm install
+rustup target add x86_64-pc-windows-msvc
+
+just build-publish
+
+cd $RootPath/target/$Job
+mv release mach
+tar -czvf mach-$Job.tar.gz mach
+mv mach-$Job.tar.gz $RootPath/artifacts
+
+cd ${{ github.workspace }}/npm/mach-os-arch
+npm pack
+mv *.tgz npm-mach-$Job.tgz
+mv *.tgz $RootPath/artifacts/npm-mach-$Job.tgz
--- .github/workflows/release/windows-arm64.ps1
+++ .github/workflows/release/windows-arm64.ps1
@@ -1,1 +1,21 @@
-echo hi
+echo hi$RootPath = @(Split-Path @(Split-Path @(Split-Path $pwd.Path)))
+$Job = "windows-arm64"
+
+"../../../"
+& "$PSScriptRoot\..\platform\windows\setup.ps1"
+
+npm install -g npm pnpm
+pnpm install
+rustup target add aarch64-pc-windows-msvc
+
+just build-publish
+
+cd $RootPath/target/$Job
+mv release mach
+tar -czvf mach-$Job.tar.gz mach
+mv mach-$Job.tar.gz $RootPath/artifacts
+
+cd ${{ github.workspace }}/npm/mach-os-arch
+npm pack
+mv *.tgz npm-mach-$Job.tgz
+mv *.tgz $RootPath/artifacts/npm-mach-$Job.tgz
