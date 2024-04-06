6265f01abef82579cb573c13d115a5f341ea3c2a
David Alsh <alshdavid@gmail.com>
2024-04-06 17:20:05 +1100

    workflow

---
+++ .github/workflows/platform/install-nodejs.bash
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Default to LTS
+NODE_VERSION="$1"
+if [ "$NODE_VERSION" = "" ]; then
+  NODE_VERSION="20"
+fi 
+
+# Default to home directory
+OUT_DIR="$2"
+if [ "$OUT_DIR" = "" ]; then
+  OUT_DIR="$HOME/.local/nodejs"
+fi 
+
+NODE_VERSION=$(curl -sSL https://nodejs.org/download/release/ |  sed -E 's/<a.*>(v.*\..*\.[0-9]+\/)<\/a>.*/\1/g' |  grep "^v" | sed -E "s/v(.*)\//\1/g" | sort -u -k 1,1n -k 2,2n -k 3,3n -t . | grep "^${NODE_VERSION}" | tail -n1)
+
+URL=""
+ARCH=""
+OS=""
+
+case $(uname -m) in
+  x86_64 | x86-64 | x64 | amd64)
+    ARCH="amd64"
+  ;;
+  aarch64 | arm64)
+    ARCH="arm64"
+  ;;
+esac
+
+case $(uname -s) in
+  Darwin)
+    OS="macos"
+  ;;
+  Linux)
+    OS="linux"
+  ;;
+esac
+
+case "$OS-$ARCH" in
+  linux-amd64)
+    URL=https://nodejs.org/download/release/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.gz
+  ;;
+  linux-arm64)
+    URL=https://nodejs.org/download/release/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-arm64.tar.gz
+  ;;
+  macos-amd64)
+    URL=https://nodejs.org/download/release/v${NODE_VERSION}/node-v${NODE_VERSION}-darwin-x64.tar.gz
+  ;;
+  macos-arm64)
+    URL=https://nodejs.org/download/release/v${NODE_VERSION}/node-v${NODE_VERSION}-darwin-arm64.tar.gz
+  ;;
+esac
+
+if [ "$URL" == "" ]; then
+  echo "Cannot find installer for Nodejs"
+  exit 1
+fi
+
+echo $URL
+mkdir -p $HOME/.local/nodejs
+curl -L --url $URL | tar -xvzf - -C $HOME/.local/nodejs --strip-components=1
+
+export PATH=$HOME/.local/nodejs/bin:$PATH
+
+npm install -g pnpm npm
---
+++ .github/workflows/platform/install-rust.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export RUSTUP_HOME=$HOME/.local/rust/rustup
+export CARGO_HOME=$HOME/.local/rust/cargo
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
+
+export PATH="$HOME/.local/rust/cargo/bin:$PATH"
