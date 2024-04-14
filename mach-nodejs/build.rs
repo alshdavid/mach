38f769a26faca214f002e62fe02f4062bbccc1fd
David Alsh <alshdavid@gmail.com>
2024-04-15 09:08:17 +1000

    napi-rs

--- mach-nodejs/build.rs
+++ mach-nodejs/build.rs
@@ -1,4 +1,4 @@
-extern crate napi_build;
+use napi_build;
 
 fn main() {
   napi_build::setup();
--- mach-nodejs/src/lib.rs
+++ mach-nodejs/src/lib.rs
@@ -1,5 +1,4 @@
-#[macro_use]
-extern crate napi_derive;
+use napi_derive::napi;
 
 #[napi]
 pub fn sum(a: i32, b: i32) -> i32 {
