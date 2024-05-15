b74582fb8b186868c589c28c8a2c96075d6a18c9
David Alsh <12656294+alshdavid@users.noreply.github.com>
2024-05-15 10:04:40 +0700

    Added JSON transformer (#12)
    * added json transformer
    
    * formatting

---
+++ mach/src/platform/plugins/transformer_json/transformer.rs
@@ -0,0 +1,21 @@
+use crate::public::MachConfig;
+use crate::public::MutableAsset;
+use crate::public::Transformer;
+
+#[derive(Debug)]
+pub struct TransformerJson {}
+
+impl Transformer for TransformerJson {
+  fn transform(
+    &self,
+    asset: &mut MutableAsset,
+    _config: &MachConfig,
+  ) -> Result<(), String> {
+    let code = asset.get_code();
+
+    asset.set_code(&format!("export default ({})", code));
+    *asset.kind = "js".to_string();
+
+    return Ok(());
+  }
+}
