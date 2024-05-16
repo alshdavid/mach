e8f0504bbe39a476a57046f5c4389532b03dfc05
David Alsh <alshdavid@gmail.com>
2024-05-16 20:43:52 +1000

    updated tests

--- testing/utils/mach/build.ts
+++ testing/utils/mach/build.ts
@@ -1,54 +1,6 @@
 import path from 'node:path';
-import child_process from 'node:child_process';
-import fs from 'node:fs';
 import * as url from 'node:url'
-import fsAsync from 'node:fs/promises';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 export const FIXTURES = (...segments: string[]) => path.resolve(__dirname, '..', '..', 'fixtures', ...segments)
-
-export type BuildOptions = {
-  cwd: string,
-  entries?: string[],
-}
-
-export type BuildReport = {
-  assets: Record<string, Buffer>
-}
-
-export async function build_mach(options: BuildOptions): Promise<BuildReport> {
-  const mach_bin = path.join(options.cwd, 'node_modules', '.bin', 'mach')
-  const entries = options.entries?.join(' ') || ''
-
-  if (!fs.existsSync(options.cwd)) {
-    throw new Error(`Error Does Not Exist: ${options.cwd}`)
-  }
-
-  if (!fs.existsSync(path.join(options.cwd, 'node_modules'))) {
-    await new Promise((resolve, reject) => {
-      child_process.exec(`npm install --no-package-lock`, { cwd: options.cwd }, (err, stdout) => {
-        if (err) return reject(err)
-        resolve(stdout)
-      })
-    })
-  }
-
-  await new Promise((resolve, reject) => {
-    child_process.exec(`${mach_bin} build -c ${entries}`, { cwd: options.cwd }, (err, stdout) => {
-      if (err) return reject(err)
-      resolve(stdout)
-    })
-  })
-  const assets: Record<string, Buffer> = {}
-
-  for (const filename of await fsAsync.readdir(path.join(options.cwd, 'dist'))) {
-    Object.defineProperty(assets, filename, { 
-      enumerable: true,
-      get: () => fs.readFileSync(path.join(options.cwd, 'dist', filename)) 
-    })
-  }
-
-  return {
-    assets
-  }
-}
+export const FIXTURES_FN = (...base_segments: string[]) => (...segments: string[]) => path.resolve(__dirname, '..', '..', 'fixtures', ...base_segments, ...segments)
