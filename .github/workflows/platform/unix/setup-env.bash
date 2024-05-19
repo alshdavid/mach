138f467cf49e4b63650df70ca78c881c08ebb608
David Alsh <alshdavid@gmail.com>
2024-05-19 13:41:00 +0700

    ci

--- .github/workflows/platform/unix/setup-env.bash
+++ .github/workflows/platform/unix/setup-env.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ev
+set -e
 
 # Just
 export PATH="${HOME}/.local/just:$PATH"
--- .github/workflows/platform/windows/install-nodejs.ps1
+++ .github/workflows/platform/windows/install-nodejs.ps1
@@ -7,7 +7,7 @@ New-Item -ItemType "directory" -Force -Path "$HOME\.local\nodejs\pnpm-store" | O
 Invoke-WebRequest https://nodejs.org/download/release/v20.2.0/node-v20.2.0-win-x64.zip -OutFile $HOME\.local\nodejs\node.zip
 
 Expand-Archive $HOME\.local\nodejs\node.zip -DestinationPath $HOME\.local\nodejs
-Move-Item $HOME\.local\nodejs\node-v20.12.1-win-x64\* $HOME\.local\nodejs
+Move-Item $HOME\.local\nodejs\node-v20.20.0-win-x64\* $HOME\.local\nodejs
 
 $env:Path = $HOME + '\.local\nodejs;' + $env:Path
 $env:Path = $HOME + '\.local\nodejs\prefix;' + $env:Path
