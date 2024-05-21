2e2f0496916635f4804b355a1ce21cf5acbff617
David Alsh <alshdavid@gmail.com>
2024-05-21 14:50:29 +0700

    ci

--- .github/workflows/release/publish-crate.bash
+++ .github/workflows/release/publish-crate.bash
@@ -5,8 +5,6 @@ JOB_NAME="linux-amd64"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIR=$(dirname $(dirname $(dirname $SCRIPT_DIR)))
 
-source $ROOT_DIR/.github/workflows/platform/unix/setup.bash
-
 if [ "$BRANCH_NAME" == "main" ]; then
   just build-publish
   cargo publish --allow-dirty --package mach_bundler_core --token $CRATES_IO_API_TOKEN
