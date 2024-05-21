9850b5e45558c89d23f527c436a8d67e6d35ab7f
David Alsh <alshdavid@gmail.com>
2024-05-21 10:33:28 +0700

    ci

--- .github/workflows/release/publish-npm-package.bash
+++ .github/workflows/release/publish-npm-package.bash
@@ -6,28 +6,28 @@ ROOT_DIR=$(dirname $(dirname $(dirname $SCRIPT_DIR)))
 ls -l "$ROOT_DIR"
 ls -l "$ROOT_DIR/artifacts"
 ls -l "$ROOT_DIR/artifacts/npm-package"
-ls -l "$ROOT_DIR/artifacts/mach-linux-amd64"
+ls -l "$ROOT_DIR/artifacts/mach-linux-amd64/npm-mach-linux-amd64.tgz"
 
-# source $ROOT_DIR/.github/workflows/platform/unix/install-nodejs.bash
+source $ROOT_DIR/.github/workflows/platform/unix/install-nodejs.bash
 
-# echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> $HOME/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> $HOME/.npmrc
 
-# PACKAGES=(
-#   "$ROOT_DIR/artifacts/npm-package/npm-mach.tgz"
-#   "$ROOT_DIR/artifacts/mach-linux-amd64/npm-mach-linux-amd64.tgz"
-#   "$ROOT_DIR/artifacts/mach-linux-arm64/npm-mach-linux-arm64.tgz"
-#   "$ROOT_DIR/artifacts/mach-macos-amd64/npm-mach-macos-amd64.tgz"
-#   "$ROOT_DIR/artifacts/mach-macos-arm64/npm-mach-macos-arm64.tgz"
-#   "$ROOT_DIR/artifacts/mach-windows-amd64/npm-mach-windows-amd64.tgz"
-#   "$ROOT_DIR/artifacts/mach-windows-arm64/npm-mach-windows-arm64.tgz"
-# )
+PACKAGES=(
+  "$ROOT_DIR/artifacts/npm-package/npm-mach.tgz"
+  "$ROOT_DIR/artifacts/mach-linux-amd64/npm-mach-linux-amd64.tgz"
+  # "$ROOT_DIR/artifacts/mach-linux-arm64/npm-mach-linux-arm64.tgz"
+  # "$ROOT_DIR/artifacts/mach-macos-amd64/npm-mach-macos-amd64.tgz"
+  # "$ROOT_DIR/artifacts/mach-macos-arm64/npm-mach-macos-arm64.tgz"
+  # "$ROOT_DIR/artifacts/mach-windows-amd64/npm-mach-windows-amd64.tgz"
+  # "$ROOT_DIR/artifacts/mach-windows-arm64/npm-mach-windows-arm64.tgz"
+)
 
-# for PACKAGE in ${PACKAGES[@]}; do
-#   if [ "$BRANCH_NAME" == "main" ]; then
-#     echo Publishing latest tag
-#     npm publish --access=public $PACKAGE
-#   else
-#     echo Publishing $BRANCH_NAME tag
-#     npm publish --access=public --tag="$BRANCH_NAME" $PACKAGE
-#   fi
-# done
+for PACKAGE in ${PACKAGES[@]}; do
+  if [ "$BRANCH_NAME" == "main" ]; then
+    echo Publishing latest tag
+    npm publish --access=public $PACKAGE
+  else
+    echo Publishing $BRANCH_NAME tag
+    npm publish --access=public --tag="$BRANCH_NAME" $PACKAGE
+  fi
+done
