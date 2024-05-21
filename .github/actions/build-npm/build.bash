b720a7f7cbea0ef353a2fcec4fec24fc0217caf6
David Alsh <alshdavid@gmail.com>
2024-05-21 09:34:14 +0700

    ci

--- .github/actions/build-npm/build.bash
+++ .github/actions/build-npm/build.bash
@@ -3,8 +3,6 @@ set -ev
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIR=$(dirname $(dirname $(dirname $SCRIPT_DIR)))
 
-source $ROOT_DIR/.github/workflows/platform/unix/setup-env.bash
-
 if ! [ "$MACH_VERSION" = "" ]; then
   echo "Building with version $MACH_VERSION"
   just build-publish
@@ -12,9 +10,3 @@ else
   echo "Building untagged local version"
   just build
 fi
-
-mkdir $ROOT_DIR/artifacts
-cd npm/mach
-npm pack
-mv *.tgz npm-mach.tgz
-mv *.tgz $ROOT_DIR/artifacts/npm-mach.tgz
