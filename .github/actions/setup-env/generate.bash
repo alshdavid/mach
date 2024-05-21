ce8dba4a9f2f229bddce4f28c99979e8009a0883
David Alsh <alshdavid@gmail.com>
2024-05-21 11:44:36 +0700

    ci

--- .github/actions/setup-env/generate.bash
+++ .github/actions/setup-env/generate.bash
@@ -3,9 +3,6 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIR=$(dirname $(dirname $(dirname $SCRIPT_DIR)))
 
-ls -l $ROOT_DIR
-cd $ROOT_DIR
-
 pnpm install
           
 export BRANCH_NAME=${GITHUB_REF##*/}
@@ -19,9 +16,9 @@ NEXT_MACH_VERSION="$(node $ROOT_DIR/.github/scripts/ci/next-npm-version.mjs)"
 
 touch "$ROOT_DIR/.env"
 
-echo "NEXT_MACH_VERSION=$NEXT_MACH_VERSION" >> "$ROOT_DIR/release.env"
-echo "BRANCH_NAME=$BRANCH_NAME" >> "$ROOT_DIR/release.env"
-echo "NPM_TAG=$NPM_TAG" >> "$ROOT_DIR/release.env"
+echo "NEXT_MACH_VERSION=$NEXT_MACH_VERSION" >> "$ROOT_DIR/.env"
+echo "BRANCH_NAME=$BRANCH_NAME" >> "$ROOT_DIR/.env"
+echo "NPM_TAG=$NPM_TAG" >> "$ROOT_DIR/.env"
 
 echo NEXT_MACH_VERSION = $NEXT_MACH_VERSION
 echo NPM_TAG = $NPM_TAG
