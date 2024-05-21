3ee9d292b8365378308f6b86931e464995aaa90a
David Alsh <alshdavid@gmail.com>
2024-05-21 09:33:13 +0700

    ci

--- .github/workflows/release/publish-npm-package.bash
+++ .github/workflows/release/publish-npm-package.bash
@@ -7,17 +7,14 @@ source $ROOT_DIR/.github/workflows/platform/unix/install-nodejs.bash
 
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> $HOME/.npmrc
 
-ls -l $ROOT_DIR/artifacts
-ls -l $ROOT_DIR/artifacts/npm-package
-
 PACKAGES=(
   "$ROOT_DIR/artifacts/npm-package/npm-mach.tgz"
-  "$ROOT_DIR/artifacts/linux-amd64/npm-mach-linux-amd64.tgz"
-  "$ROOT_DIR/artifacts/linux-arm64/npm-mach-linux-arm64.tgz"
-  "$ROOT_DIR/artifacts/macos-amd64/npm-mach-macos-amd64.tgz"
-  "$ROOT_DIR/artifacts/macos-arm64/npm-mach-macos-arm64.tgz"
-  "$ROOT_DIR/artifacts/windows-amd64/npm-mach-windows-amd64.tgz"
-  "$ROOT_DIR/artifacts/windows-arm64/npm-mach-windows-arm64.tgz"
+  "$ROOT_DIR/artifacts/mach-linux-amd64/npm-mach-linux-amd64.tgz"
+  "$ROOT_DIR/artifacts/mach-linux-arm64/npm-mach-linux-arm64.tgz"
+  "$ROOT_DIR/artifacts/mach-macos-amd64/npm-mach-macos-amd64.tgz"
+  "$ROOT_DIR/artifacts/mach-macos-arm64/npm-mach-macos-arm64.tgz"
+  "$ROOT_DIR/artifacts/mach-windows-amd64/npm-mach-windows-amd64.tgz"
+  "$ROOT_DIR/artifacts/mach-windows-arm64/npm-mach-windows-arm64.tgz"
 )
 
 for PACKAGE in ${PACKAGES[@]}; do
