22763c558af81a942666e8512e0e8960808bc3f4
David Alsh <alshdavid@gmail.com>
2024-06-13 15:42:24 +1000

    comment out snapshot generator

--- packages/mach/src/platform/transformation/testing/test_update.rs
+++ packages/mach/src/platform/transformation/testing/test_update.rs
@@ -2,7 +2,7 @@ use super::snapshot::generate_project_snapshot;
 use super::utils::FIXTURES;
 
 // Uncomment this and run it to update snapshots
-#[test]
+// #[test]
 fn _test_update_snapshots() {
   for dir in std::fs::read_dir(&*FIXTURES).unwrap() {
     let project_root = dir.unwrap().path();
